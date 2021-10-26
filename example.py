c90a1c19f7c314ca2a98036e19cf41573c211b16
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2021-10-26 10:48:47 -0300

    Limit SIFT

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ from modules.utils import DEAL #include the modules on your path
 if __name__ == "__main__":
     net_path = 'weights/newdata-DEAL-big.pth' # weight path
     nrlfeat = DEAL(net_path, sift = True) # Create the descriptor and load arctecture
-    sift = cv2.SIFT_create()
+    sift = cv2.SIFT_create(2048)
 
     img_path = "./example/notredame.png"
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
